195162882bbea00648f300410b9b4a0bca307e15
Ze_Moreira <Careca@NB-CARECA>
2020-07-12 11:27:52 -0300

    change port

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: "https://grades-api-12-10-1981.herokuapp.com/",
+  baseURL: "https://grades-api-12-10-1981.herokuapp.com:443/",
   headers: {
     "Content-type": "application/json",
   },
